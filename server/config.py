206d6f1faee40e520530a7dfec0d64af6271e1f8
george <george.benedict@student.moringaschool.com>
2025-06-23 21:41:05 +0300

    confihgure flask to use database

---
+++ server/config.py
@@ -0,0 +1,5 @@
+import os
+
+SQLALCHEMY_DATABASE_URI = "postgresql://george-benedict:12345@localhost:5432/late_show_db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret")
