ef6b08e18e1ef6d5776b26fdab92bca94a3deea8
george <george.benedict@student.moringaschool.com>
2025-06-23 23:06:19 +0300

    migrations running updating config.py

--- server/config.py
+++ server/config.py
@@ -1,5 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+
 import os
 
-SQLALCHEMY_DATABASE_URI = "postgresql://george-benedict:12345@localhost:5432/late_show_db"
+db = SQLAlchemy()
+jwt = JWTManager()
+
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "postgresql://george-benedict:12345@localhost:5432/late_show_db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret")
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "super-secret"
